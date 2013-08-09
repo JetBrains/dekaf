06679dc04b814a3ebd13d6b6e31fc054b6c034e1
Leonid Bushuev <leonidos@gmail.com>
2013-08-09 08:52:40 +0200

    A simple common script

---
+++ DBA/tests-sql/sql/common/simple-script.sql
@@ -0,0 +1,16 @@
+-- A simple script that can be executable on all supported RDBMS.
+
+create table Simple_Table
+(
+    Prop_Code varchar(6) not null primary key,
+    Prop_Value nvarchar(80)
+);
+
+insert into Simple_Table values ('P1', 'Aaa');
+
+insert into Simple_Table values ('P2', 'Bbb');
+
+commit;
+
+drop table Simple_Table;
+
