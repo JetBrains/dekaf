7e66c6d01c372910bfa90c45f7aa2189561cef0d
Leonid Bushuev <leonidos@gmail.com>
2013-08-22 16:57:35 +0200

    Join SQL scripts

--- DBA/src/org/jetbrains/dba/sql/SQLScript.java
+++ DBA/src/org/jetbrains/dba/sql/SQLScript.java
@@ -26,6 +26,18 @@ public class SQLScript {
     this(ImmutableList.copyOf(commands));
   }
 
+  public SQLScript(@NotNull final SQLScript... scripts) {
+    this(joinCommands(scripts));
+  }
+
+  private static ImmutableList<SQLCommand> joinCommands(SQLScript[] scripts) {
+    ImmutableList.Builder<SQLCommand> b = ImmutableList.builder();
+    for (SQLScript script : scripts) {
+      b.addAll(script.getCommands());
+    }
+    return b.build();
+  }
+
 
   SQLScript(@NotNull final ImmutableList<SQLCommand> commands) {
     this.commands = commands;
@@ -35,5 +47,33 @@ public class SQLScript {
   @NotNull
   public List<SQLCommand> getCommands() {
     return commands;
+  }
+
+
+  @Override
+  @NotNull
+  public String toString() {
+    int n = commands.size();
+    switch (n) {
+      case 0: return "";
+      case 1: return commands.get(0).getSourceText();
+      default:
+        final StringBuilder b = new StringBuilder();
+        final String delimiterString = getScriptDelimiterString();
+        b.append(commands.get(0).getSourceText());
+        for (int i = 1; i < n; i++) {
+          if (b.charAt(b.length()-1) != '\n')  b.append('\n');
+          b.append(delimiterString).append('\n');
+          b.append(commands.get(i).getSourceText());
+        }
+        return b.toString();
+    }
+    // TODO cache it
+  }
+
+
+  @NotNull
+  protected String getScriptDelimiterString() {
+    return ";";
   }
 }
--- DBA/tests/org/jetbrains/dba/sql/SQLScriptTest.java
+++ DBA/tests/org/jetbrains/dba/sql/SQLScriptTest.java
@@ -4,8 +4,7 @@ import org.testng.annotations.Test;
 
 import java.util.Arrays;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertSame;
+import static org.testng.Assert.*;
 
 
 
@@ -34,6 +33,30 @@ public class SQLScriptTest {
     assertEquals(script.getCommands().size(), 2);
     assertSame(script.getCommands().get(0), cmd1);
     assertSame(script.getCommands().get(1), cmd2);
+  }
+
+  @Test
+  public void construct_3() {
+    SQLCommand cmd1 = sql.command("command1"),
+               cmd2 = sql.command("command2"),
+               cmd3 = sql.command("command3"),
+               cmd4 = sql.command("command4");
+    SQLScript script12 = new SQLScript(cmd1, cmd2),
+              script34 = new SQLScript(cmd3, cmd4);
+    SQLScript script = new SQLScript(script12, script34);
+    assertEquals(script.getCommands().size(), 4);
+    assertSame(script.getCommands().get(0), cmd1);
+    assertSame(script.getCommands().get(1), cmd2);
+    assertSame(script.getCommands().get(2), cmd3);
+    assertSame(script.getCommands().get(3), cmd4);
+  }
+
+  @Test
+  public void toString_contains_all_commands() {
+    SQLScript script = sql.script("command 1", "command 2");
+    final String text = script.toString();
+    assertTrue(text.contains("command 1"));
+    assertTrue(text.contains("command 2"));
   }
 
 }
