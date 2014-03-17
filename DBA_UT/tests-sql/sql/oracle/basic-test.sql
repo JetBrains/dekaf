6f063a5065095f79a6db5fd61358e418bfa570a5
Leonid Bushuev <leonidos@gmail.com>
2014-03-17 13:08:07 +0400

    Use Oracle end markers in some test compound SQL texts

--- DBA_UT/tests-sql/sql/oracle/basic-test.sql
+++ DBA_UT/tests-sql/sql/oracle/basic-test.sql
@@ -3,23 +3,25 @@ create table Basic_Table
 (
     Basic_Id number(9) not null primary key,
     Basic_Name varchar(25)
-);
+)
+/
 
 
 --=-- InsertIntoTable
 insert
     into Basic_Table
-    values (44, 'Forty Four');
-
+    values (44, 'Forty Four')
+/
 
 --=-- SelectFromTable
 select *
 from Basic_Table
-order by 1;
-
+order by 1
+/
 
 --=-- DropTable
-drop table Basic_Table;
+drop table Basic_Table
+/
 
 
 
