d71040f40498853ecb75f340589fc4ae114c489c
Leonid Bushuev <leonidos@gmail.com>
2015-05-18 12:02:22 +0200

    Ignore tests on direct class fetching while this functionality is not implemented yet

--- jdba-core-dba/src/test/java/org/jetbrains/jdba/core/BaseQueryRunnerDirectTest.java
+++ jdba-core-dba/src/test/java/org/jetbrains/jdba/core/BaseQueryRunnerDirectTest.java
@@ -1,7 +1,12 @@
 package org.jetbrains.jdba.core;
 
+import org.junit.Ignore;
+
+
+
 /**
  * @author Leonid Bushuev from JetBrains
  */
+@Ignore // TODO remove the @Ignore when tge direct class fetching is done
 public class BaseQueryRunnerDirectTest extends BaseQueryRunnerTest {
 }
