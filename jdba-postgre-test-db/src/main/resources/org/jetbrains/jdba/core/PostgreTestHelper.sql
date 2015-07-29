cbba8adb669f3c9748e43c37046a0c2f87a07ef3
Leonid Bushuev <leonidos@gmail.com>
2015-07-29 16:00:23 +0200

    Postgre Base Test Helper: unify zap meta-query

--- jdba-postgre-test-db/src/main/resources/org/jetbrains/jdba/core/PostgreTestHelper.sql
+++ jdba-postgre-test-db/src/main/resources/org/jetbrains/jdba/core/PostgreTestHelper.sql
@@ -26,61 +26,42 @@ where table_catalog = current_database()
 ---- ZapSchemaMetaQuery ----
 with N as ( select min(oid) as n_id
             from pg_catalog.pg_namespace
-            where nspname = current_schema )
+            where nspname = current_schema
+            limit 1 )
 --
-select concat('drop sequence if exists ', relname, ' cascade') as cmd,
-       1 as priority, oid::varchar::bigint as ord
-from pg_catalog.pg_class
+select concat('drop ', what, ' if exists ', typname, ' cascade') as cmd,
+       T.oid::varchar::bigint as ord
+from pg_catalog.pg_type T
+     natural join
+     (values ('e'::"char", 'type'),('d'::"char", 'domain')) as TX(typtype, what)
+where typnamespace = (select n_id from N)
+--
+union all
+--
+select concat('drop type if exists ', ST.typname, ' cascade') as cmd,
+       ST.oid::varchar::bigint as ord
+from pg_catalog.pg_type ST,
+     pg_catalog.pg_class SC
+where ST.typnamespace = (select n_id from N)
+  and ST.typrelid = SC.oid
+  and SC.relkind = 'c'::"char"
+--
+union all
+--
+select concat('drop ', what, ' if exists ', relname, ' cascade') as cmd,
+       C.oid::varchar::bigint as ord
+from pg_catalog.pg_class C
+     natural join
+     (values ('S'::"char", 'sequence'),('r'::"char", 'table'),('v'::"char", 'view')) as CX(relkind, what)
 where relnamespace = (select n_id from N)
-  and relkind = 'S'::"char"
---
-union all
---
-select concat('drop type if exists ', typname, ' cascade') as cmd,
-       2 as priority, oid::varchar::bigint as ord
-from pg_catalog.pg_type
-where typtype = 'e'::"char"
---
-union all
---
-select concat('drop type if exists ', T.typname, ' cascade') as cmd,
-       3 as priority, T.oid::varchar::bigint as ord
-from pg_catalog.pg_type T,
-     pg_catalog.pg_class C
-where T.typnamespace = (select n_id from N)
-  and T.typrelid = C.oid
-  and C.relkind = 'c'::"char"
-union all
---
-select concat('drop domain if exists ', typname, ' cascade') as cmd,
-       4 as priority, oid::varchar::bigint as ord
-from pg_catalog.pg_type
-where typnamespace in (select n_id from N)
-  and typtype = 'd'::"char"
---
-union all
---
-select concat('drop table if exists ', relname, ' cascade') as cmd,
-       5 as priority, oid::varchar::bigint as ord
-from pg_catalog.pg_class
-where relnamespace = (select n_id from N)
-  and relkind = 'r'::"char"
---
-union all
---
-select concat('drop view if exists ', relname, ' cascade') as cmd,
-       6 as priority, oid::varchar::bigint as ord
-from pg_catalog.pg_class
-where relnamespace = (select n_id from N)
-  and relkind = 'v'::"char"
 --
 union all
 --
 select concat('drop function if exists ', proname, '(', oidvectortypes(proargtypes), ') cascade') as cmd,
-       7 as priority, oid::varchar::bigint as ord
+       oid::varchar::bigint as ord
 from pg_catalog.pg_proc
 where pronamespace = (select n_id from N)
 --
-order by priority desc, ord desc
+order by ord desc
 ;
 
