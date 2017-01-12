5398b47293d2145d26492ef8992a617baca87226
Alexander Kass <Alexander.Kass@jetbrains.com>
2017-01-12 17:42:32 +0300

    Sqlite support empty cursor

--- dekaf-sqlite/src/main/java/org/jetbrains/dekaf/jdbc/SqliteIntermediateSession.java
+++ dekaf-sqlite/src/main/java/org/jetbrains/dekaf/jdbc/SqliteIntermediateSession.java
@@ -7,9 +7,16 @@ import org.jetbrains.dekaf.intermediate.DBExceptionRecognizer;
 import java.sql.Connection;
 
 public class SqliteIntermediateSession extends JdbcIntermediateSession {
-  protected SqliteIntermediateSession(@Nullable JdbcIntermediateFacade facade,
-                                      @NotNull DBExceptionRecognizer exceptionRecognizer,
-                                      @NotNull Connection connection, boolean ownConnection) {
+
+  SqliteIntermediateSession(@Nullable JdbcIntermediateFacade facade,
+                            @NotNull DBExceptionRecognizer exceptionRecognizer,
+                            @NotNull Connection connection, boolean ownConnection) {
     super(facade, exceptionRecognizer, connection, ownConnection);
+  }
+
+  @NotNull
+  @Override
+  protected JdbcIntermediateSimpleSeance openSimpleStatementSeance(@NotNull String statementText) {
+    return new SqliteIntermediateSimpleSeance(this, statementText);
   }
 }
