99fbd348f80da54612980840a785718063ba71d2
dmitriy.tseyler <dmitriy.tseyler@jetbrains.com>
2017-05-19 19:15:50 +0300

    Redshift support

--- dekaf-redshift/src/main/java/org/jetbrains/dekaf/Redshift.java
+++ dekaf-redshift/src/main/java/org/jetbrains/dekaf/Redshift.java
@@ -2,6 +2,6 @@ package org.jetbrains.dekaf;
 
 public final class Redshift {
 
-  public static final Rdbms RDBMS = Rdbms.of("Redshift");
+  public static final Rdbms RDBMS = Rdbms.of("REDSHIFT");
 
 }
