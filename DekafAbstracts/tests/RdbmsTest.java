c0e9e2689d88183473af6f799b2bdd718961fa45
Leonid Bushuev <leonidos@gmail.com>
2017-08-02 14:31:00 +0200

    DekafAbstracts: getting rid of Kotlin tests

--- DekafAbstracts/tests/RdbmsTest.java
+++ DekafAbstracts/tests/RdbmsTest.java
@@ -2,6 +2,8 @@ package org.jetbrains.dekaf;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.*;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -23,6 +25,29 @@ class RdbmsTest {
         assertThat(marker2).isSameAs(marker1);
         assertThat(marker2.code).isSameAs(marker1.code);
 
+    }
+
+
+
+    private static class JustAClass implements Serializable {
+        final Rdbms rdbms;
+        private JustAClass(final Rdbms rdbms) {this.rdbms = rdbms;}
+    }
+
+    @Test
+    void serialization() throws IOException, ClassNotFoundException {
+        JustAClass c1 = new JustAClass(Rdbms.of("MicroDB"));
+
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(os);
+        oos.writeObject(c1);
+
+        ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(os.toByteArray()));
+        Object gotObject = ois.readObject();
+        assertThat(gotObject).isInstanceOf(JustAClass.class);
+
+        JustAClass c2 = (JustAClass) gotObject;
+        assertThat(c2.rdbms).isSameAs(c1.rdbms);
     }
 
 }
