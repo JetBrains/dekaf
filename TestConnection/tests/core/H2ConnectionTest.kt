c8d89002d67b5731c34c04d4356159b40f997799
Leonid Bushuev <leonidos@gmail.com>
2017-09-17 03:29:44 +0300

    Tests: work in progress: (1) run basic (unit)tests from Gradle (2) introduce connection tests

---
+++ TestConnection/tests/core/H2ConnectionTest.kt
@@ -0,0 +1,23 @@
+package org.jetbrains.dekaf.core
+
+import org.jetbrains.dekaf.DekafMaster
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+
+
+@Tag("H2")
+class H2ConnectionTest {
+
+    @Test
+    fun connectMemory() {
+        val connectString1 = "jdbc:h2:mem:TestDatabase1"
+        val connectString2 = "jdbc:h2:mem:TestDatabase2"
+        val facade1 = DekafMaster.provider.provide(connectString1)
+        val facade2 = DekafMaster.provider.provide(connectString2)
+        facade1.connect()
+        facade2.connect()
+        facade1.disconnect()
+        facade2.disconnect()
+    }
+
+}
