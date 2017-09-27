edb80983914bea15444fee834881a385c99685cd
Leonid Bushuev <leonidos@gmail.com>
2017-09-27 18:57:05 +0200

    Introduce SQLM grammar using AntLR — integrate the Java code

---
+++ DekafMain/parser/org/jetbrains/dekaf/parser/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * SQLM parser, generated using AntLR 4.
+ *
+ * <p>
+ * In order to re-generate the parser, perfrom the following:
+ * <pre>
+ *     cd DekafMain/grammar
+ *     antlr4 SQLM.g4 -package org.jetbrains.dekaf.parser -o ../parser/org/jetbrains/dekaf/parser/
+ *     cd -
+ * </pre>
+ * </p>
+ */
+package org.jetbrains.dekaf.parser;
