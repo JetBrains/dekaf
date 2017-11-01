3b975eba9e0d17e6a60666149700f08db338e4e3
Leonid Bushuev <leonidos@gmail.com>
2017-11-01 13:13:10 +0100

    Split every module into two ones: production and tests, in order to workaround the Gradle conception problem (Gradle doesn't handle test dependency jars like production ones).

---
+++ dekaf-main-test/tests/sql/SqlCommandTest.kt
@@ -0,0 +1,22 @@
+package org.jetbrains.dekaf.sql
+
+
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.dekaf.text.Rewriters
+import org.junit.jupiter.api.Test
+
+
+class SqlCommandTest {
+
+    @Test
+    fun rewrite_basic() {
+        val cmd1 = SqlCommand("AAA BBB CCC", 33, "name")
+        val cmd2 = cmd1.rewrite(Rewriters.replace("BBB", "XXX"))
+
+        assertThat(cmd2.sourceText).isEqualTo("AAA XXX CCC")
+
+        assertThat(cmd2.row).isEqualTo(33)
+        assertThat(cmd2.name).isEqualTo("name")
+    }
+
+}
