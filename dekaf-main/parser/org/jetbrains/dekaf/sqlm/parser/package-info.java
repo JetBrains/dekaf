35e10c110a0fc1164584b4ba44ff2e41d25e9432
Leonid Bushuev <leonidos@gmail.com>
2018-03-23 10:44:00 +0100

    Fix compilation after merging

--- dekaf-main/parser/org/jetbrains/dekaf/sqlm/parser/package-info.java
+++ dekaf-main/parser/org/jetbrains/dekaf/sqlm/parser/package-info.java
@@ -4,7 +4,7 @@
  * <p>
  * In order to re-generate the parser, perfrom the following:
  * <pre>
- *     cd DekafMain/grammar
+ *     cd dekaf-main/grammar
  *     antlr4 SQLM.g4 -package org.jetbrains.dekaf.sqlm.parser -o ../parser/org/jetbrains/dekaf/sqlm/parser/
  *     cd -
  * </pre>
