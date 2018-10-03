9d7966a29bd456d0c09c26897bc3e2deac74e615
Liudmila Kornilova <liudmila.kornilova@jetbrains.com>
2018-10-03 17:43:27 +0300

    Retrieve cassandra current keyspace

--- dekaf-cassandra-test-db/src/main/java/org/jetbrains/dekaf/core/CassandraTestHelper.java
+++ dekaf-cassandra-test-db/src/main/java/org/jetbrains/dekaf/core/CassandraTestHelper.java
@@ -1,14 +1,21 @@
 package org.jetbrains.dekaf.core;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.dekaf.sql.Scriptum;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 
 
 public class CassandraTestHelper extends BaseTestHelper<DBFacade> {
+  private final Connection connection;
 
   public CassandraTestHelper(@NotNull final DBFacade db) {
     super(db, Scriptum.of(CassandraTestHelper.class));
+    connection = db.getSpecificService(Connection.class,
+                                       ImplementationAccessibleService.Names.JDBC_CONNECTION);
   }
 
   @Override
@@ -30,14 +37,34 @@ public class CassandraTestHelper extends BaseTestHelper<DBFacade> {
   @Override
   protected void zapSchemaInternally(final ConnectionInfo connectionInfo) {
     performCommand(scriptum, "CreateDropFunction");
-    super.zapSchemaInternally(connectionInfo);
+    performMetaQueryCommands(scriptum, "ZapSchemaMetaQuery", getCurrentKeyspace());
     performCommand(scriptum, "DropDropFunction");
   }
 
   @Override
   protected void ensureNoTableOrView4(final Object[] params) {
     performCommand(scriptum, "CreateDropFunction");
-    super.ensureNoTableOrView4(params);
+    super.ensureNoTableOrView4(prependParam(getCurrentKeyspace(), params));
     performCommand(scriptum, "DropDropFunction");
+  }
+
+  @NotNull
+  private String getCurrentKeyspace() {
+    try {
+      String keyspace = connection.getCatalog();
+      if (keyspace == null) throw new IllegalStateException("No keyspace is selected");
+      return keyspace;
+    }
+    catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private Object[] prependParam(@Nullable String param, @NotNull Object[] params) {
+    if (param == null) return params;
+    Object[] newParams = new Object[params.length + 1];
+    newParams[0] = param;
+    System.arraycopy(params, 0, newParams, 1, params.length);
+    return newParams;
   }
 }
--- dekaf-cassandra-test-db/src/main/resources/org/jetbrains/dekaf/core/CassandraTestHelper.sql
+++ dekaf-cassandra-test-db/src/main/resources/org/jetbrains/dekaf/core/CassandraTestHelper.sql
@@ -19,12 +19,12 @@ DROP FUNCTION IF EXISTS create_drop_statement
 ---- ZapSchemaMetaQuery ----
 SELECT create_drop_statement(table_name) as cmd
 FROM system_schema.tables
-where keyspace_name = schema();
+where keyspace_name = ?;
 ;
 
 ---- EnsureNoTableOrViewMetaQuery ----
 SELECT create_drop_statement(table_name) as cmd
 FROM system_schema.tables
-where keyspace_name = schema() AND
+where keyspace_name = ? AND
 table_name in (?, ?, ?, ?)
 ;
