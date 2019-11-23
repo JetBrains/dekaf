a06d64a0341115ac1c1c754b62d27c5fdf0acbdd
Leonid Bushuev <leonidos@gmail.com>
2019-11-23 03:05:01 +0300

    Settings

---
+++ dekaf-inter-test/tests/settings/SettingsTest.kt
@@ -0,0 +1,94 @@
+package com.jetbrains.dekaf.interTest.settings
+
+import lb.yaka.expectations.equalsTo
+import lb.yaka.expectations.sameAs
+import lb.yaka.gears.expect
+import org.jetbrains.dekaf.inter.settings.Setting
+import org.jetbrains.dekaf.inter.settings.Settings
+import org.jetbrains.dekaf.test.utils.UnitTest
+import org.junit.jupiter.api.Test
+
+
+class SettingsTest : UnitTest {
+
+    @Test
+    fun get_byIndex() {
+        val settings = Settings(Setting("name1", "value A"),
+                                Setting("name2", "value B"),
+                                Setting("name3", "value C"))
+        expect that settings[0] equalsTo Setting("name1", "value A")
+        expect that settings[1] equalsTo Setting("name2", "value B")
+        expect that settings[2] equalsTo Setting("name3", "value C")
+        expect that settings.size equalsTo 3
+    }
+
+    @Test
+    fun get_byName_3() {
+        val settings = Settings(Setting("name1", "value A"),
+                                Setting("name2", "value B"),
+                                Setting("name3", "value C"))
+        expect that settings["name1"] equalsTo Setting("name1", "value A")
+        expect that settings["name2"] equalsTo Setting("name2", "value B")
+        expect that settings["name3"] equalsTo Setting("name3", "value C")
+    }
+
+    @Test
+    fun get_byName_12() {
+        doTestByName(12)
+    }
+
+    @Test
+    fun get_byName_44() {
+        doTestByName(44)
+    }
+
+    private fun doTestByName(n: Int) {
+        val entries = Array(n) { i -> Setting("name$i", "value $i") }
+        val settings = Settings(*entries)
+        for (i in 0 until n) {
+            expect that settings["name$i"] sameAs entries[i]
+        }
+    }
+
+    @Test
+    fun toText_plain() {
+        val settings = Settings.of("name1", "value A",
+                                   "name2", "value B",
+                                   "name3", "value C")
+        val text = settings.toText().toString()
+        val expectedText = """|name1 = value A
+                              |name2 = value B
+                              |name3 = value C
+                           """.trimMargin() + '\n'
+        expect that text equalsTo expectedText
+    }
+
+    @Test
+    fun toText_nested() {
+        val settings2 = Settings.of("name1", "value A",
+                                    "name2", "value B",
+                                    "name3", "value C")
+        val settings1 = Settings.of("x", settings2,
+                                    "y", "another value")
+        val text = settings1.toText().toString()
+        val expectedText = """|x.name1 = value A
+                              |x.name2 = value B
+                              |x.name3 = value C
+                              |y = another value
+                           """.trimMargin() + '\n'
+        expect that text equalsTo expectedText
+    }
+
+    @Test
+    fun iterator_basic() {
+        val s1 = Setting("name1", "value A")
+        val s2 = Setting("name2", "value B")
+        val s3 = Setting("name3", "value C")
+        val settings = Settings(s1, s2, s3)
+        val it = settings.iterator()
+        expect that it.next() sameAs s1
+        expect that it.next() sameAs s2
+        expect that it.next() sameAs s3
+    }
+
+}
---
+++ doc/settings.md
@@ -0,0 +1,22 @@
+Settings
+--------
+
+Format of settings, passed to an RDBMS service facade.
+
+
+### Driver settings
+
+```      
+driver.path
+driver.jars
+driver.class
+```
+
+
+### JDBC setting
+
+```              
+jdbc.connection-string 
+jdbc.parameters.*
+```
+
