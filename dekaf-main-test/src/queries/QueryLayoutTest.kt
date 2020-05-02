f945b299b674dc26736d03f6b1cf4b5eb643c3ad
Leonid Bushuev <leonidos@gmail.com>
2020-05-03 00:01:35 +0300

    Cleanup one test

--- dekaf-main-test/src/queries/QueryLayoutTest.kt
+++ dekaf-main-test/src/queries/QueryLayoutTest.kt
@@ -1,3 +1,5 @@
+@file:Suppress("RemoveExplicitTypeArguments") // a bug in kotlin inspection
+
 package org.jetbrains.dekaf.mainTest.queries
 
 import lb.yaka.expectations.equalsTo
@@ -11,8 +13,10 @@ import org.jetbrains.dekaf.mainTest.base.UnitTestWithH2
 import org.jetbrains.dekaf.mainTest.util.*
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
+import java.util.*
 import java.util.stream.Collectors
 import java.util.stream.Stream
+import kotlin.collections.ArrayList
 
 
 class QueryLayoutTest : UnitTestWithH2 {
@@ -34,44 +38,26 @@ class QueryLayoutTest : UnitTestWithH2 {
                 }
         expect that list hasSize 4
 
-        expect that list[0] hasSize 4
-        expect that list[1] hasSize 4
-        expect that list[2] hasSize 4
-        expect that list[3] hasSize 4
-
-        expect that list[0][0] equalsTo 1
-        expect that list[0][1] equalsTo 2
-        expect that list[0][2] equalsTo 3
-        expect that list[0][3] equalsTo 4
-        expect that list[1][0] equalsTo 5
-        expect that list[1][1] equalsTo 6
-        expect that list[1][2] equalsTo 7
-        expect that list[1][3] equalsTo 8
+        checkList4x4(list)
     }
 
     @Test @Order(2)
     fun queryArrayOfArrayOfMandatoryNumber() {
         val query: Query<Array<Array<out Number>>> =
                 Query(query4x4, layTableArrayOf(layRowArrayOf<Number>(4, `0`)))
-        val list: Array<Array<out Number>> =
+        val array: Array<Array<out Number>> =
                 dbf.inSession { session ->
                     session.query(query).run()
                 }
-        expect that list hasSize 4
+        expect that array hasSize 4
 
-        expect that list[0] hasSize 4
-        expect that list[1] hasSize 4
-        expect that list[2] hasSize 4
-        expect that list[3] hasSize 4
-
-        expect that list[0][0] equalsTo 1
-        expect that list[0][1] equalsTo 2
-        expect that list[0][2] equalsTo 3
-        expect that list[0][3] equalsTo 4
-        expect that list[1][0] equalsTo 5
-        expect that list[1][1] equalsTo 6
-        expect that list[1][2] equalsTo 7
-        expect that list[1][3] equalsTo 8
+        expect that array[0] hasSize 4
+        expect that array[1] hasSize 4
+        expect that array[2] hasSize 4
+        expect that array[3] hasSize 4
+        
+        val list: List<Array<out Number>> = Arrays.asList(*array)
+        checkList4x4(list)
     }
 
     @Test @Order(3)
@@ -97,23 +83,8 @@ class QueryLayoutTest : UnitTestWithH2 {
                 }
 
         val list = ArrayList<Array<out Number>>()
-        for (element in iterator) {
-            list.add(element)
-        }
-
-        expect that list[0] hasSize 4
-        expect that list[1] hasSize 4
-        expect that list[2] hasSize 4
-        expect that list[3] hasSize 4
-
-        expect that list[0][0] equalsTo 1
-        expect that list[0][1] equalsTo 2
-        expect that list[0][2] equalsTo 3
-        expect that list[0][3] equalsTo 4
-        expect that list[1][0] equalsTo 5
-        expect that list[1][1] equalsTo 6
-        expect that list[1][2] equalsTo 7
-        expect that list[1][3] equalsTo 8
+        for (element in iterator) list.add(element)
+        checkList4x4(list)
     }
 
     @Test @Order(5)
@@ -126,6 +97,12 @@ class QueryLayoutTest : UnitTestWithH2 {
                 }
 
         val list: List<Array<out Number>> = stream.collect(Collectors.toList())
+        checkList4x4(list)
+    }
+
+
+    private fun checkList4x4(list: List<Array<out Number>>) {
+        expect that list hasSize 4
 
         expect that list[0] hasSize 4
         expect that list[1] hasSize 4
