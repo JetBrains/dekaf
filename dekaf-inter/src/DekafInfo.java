10703a8afc5f88a1a1cd200782cc0be88eb3aa08
Leonid Bushuev <leonidos@gmail.com>
2020-11-07 23:30:49 +0100

    Gradle build script

---
+++ dekaf-inter/src/DekafInfo.java
@@ -0,0 +1,14 @@
+package org.jetbrains.dekaf.inter;
+
+import org.jetbrains.dekaf.util.Version;
+
+
+
+/**
+ * Brief information.
+ */
+public class DekafInfo {
+
+  public static final Version VERSION = Version.of(3, 0, 0, -30);
+
+}
