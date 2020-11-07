a6a72c602a4ee49719421967057ea82429733a17
Leonid Bushuev <leonidos@gmail.com>
2020-11-07 21:53:08 +0100

    Ant is using to build

---
+++ dekaf-inter/src/DekafInfo.java
@@ -0,0 +1,9 @@
+package org.jetbrains.dekaf.inter;
+
+import org.jetbrains.dekaf.util.Version;
+
+final class DekafInfo {
+
+    public static final Version DEKAF_VERSION = Version.of(3, 0, 0, -30);
+
+}
---
+++ lib/README.txt
@@ -0,0 +1,9 @@
+Library files (jars).
+For downloading files invoke:
+
+    ant download-libs
+
+
+
+
+P.S. The downloaded files must not be committed into the source repository.
