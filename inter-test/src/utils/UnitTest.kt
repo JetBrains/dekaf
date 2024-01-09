d56c3e7aab0a90ba7adf8a8d9ececa0fd8faa4e2
Leonid Bushuev <leonidos@gmail.com>
2024-01-09 18:30:23 +0100

    Test modules and libraries

---
+++ inter-test/src/utils/UnitTest.kt
@@ -0,0 +1,10 @@
+package org.jetbrains.dekaf.inter.test.utils
+
+
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.TestInstance
+
+
+@Tag("UnitTest")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+interface UnitTest
